6f369c080874c5aea71db850d8f03bec76caa5c8
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-05-02 16:01:05 -0300

    Se agrega el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <headerView></headerView>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import headerView from './components/header.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components:{
+    headerView,
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/header.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app-bar app color="teal darken-3">
+        <div class="d-flex align-center">
+            <v-img alt="Woove logo" contain src="../assets/images/icons/logo_woove_white.svg" width="40"/>
+        </div>
+
+        <v-spacer></v-spacer>
+
+            <!-- Buttons type text -->
+            <router-link to="/">
+                <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text class="white--text">
+                    <span class="mr-2">Home</span>
+                    <v-icon>mdi-home</v-icon>
+                </v-btn>
+            </router-link>
+
+            <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text class="white--text">
+                <span class="mr-2">Products</span>
+                <v-icon>mdi-bag-personal</v-icon>
+            </v-btn>
+
+            <router-link to="/about">
+                <v-btn text class="white--text">
+                    <span class="mr-2">About us</span>
+                    <v-icon>mdi-account-question</v-icon>
+                </v-btn>
+            </router-link>
+
+            <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text class="white--text">
+                <span class="mr-2">Contact</span>
+                <v-icon>mdi-tooltip-account</v-icon>
+            </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'headerView',
+
+  data: () => ({
+    
+  }),
+};
+</script>
